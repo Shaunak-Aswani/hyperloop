80c1d62c7939921d7ab11c0277c72ca329cac883
shaunak <indusman9@gmail.com>
2021-01-03 22:06:16 +0800

    Added friction and velocity

--- sketch.js
+++ sketch.js
@@ -13,19 +13,37 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-    world.gravity.x = 1;
+    world.gravity.x = 0
+    ;
 
     box1 = new Box(0,410,100,40);
-    ground = new Ground(3200,400,worldLength,20)
-    ground2 = new Ground(3200,446,worldLength,20)
+    ground = new Ground(3200,400,worldLength + 100,20)
+    ground2 = new Ground(3200,446,worldLength + 100,20)
     ground3 = new Ground(worldLength+25,400,50,50)
+    resetClicked()
 }
 
 function draw(){
     background("black");
     Engine.update(engine);
-
+    console.log(box1.body.velocity.x);
     box1.display();
     ground.display();
     ground2.display();
+}
+
+function resetClicked() {
+    console.log("Setting box");
+    Matter.Body.setPosition(box1.body,{x:0,y:410});
+    Matter.Body.setVelocity(box1.body,{x:0,y:0})
+    Matter.Body.applyForce(box1.body, {x:0, y:410}, {x:30000,y:0});
+    
+}
+
+function updateFriction(f) {
+    box1.body.friction = f/1000;
+    console.log(box1.body.friction);
+
+    //box1.body.friction = 0;
+    resetClicked();
 }
